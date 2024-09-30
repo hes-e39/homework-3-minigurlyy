a846816be154136eef88dc9fff9ebc7128b53e12
minigurlyy <srzahid99@gmail.com>
2024-09-29 22:45:49 -0700

    display colors in simple manner in center of page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
-const App = () => {
-    return <h1>Hello World</h1>;
-};
+import colors from './colors.json';
+
+function App() {
+    return (
+        <div className="app">
+            <h1>Color List</h1>
+            <ul>
+                {colors.map(color => (
+                    <li key={color.name} style={{ color: color.hex }}>
+                        {color.name}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
 
 export default App;
